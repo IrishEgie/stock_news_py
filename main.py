c06ed8ccaa3815ceddf441ea5fd5cb34cd70387c
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-24 20:36:11 +0800

    update: added news api

--- main.py
+++ main.py
@@ -8,19 +8,31 @@ COMPANY_NAME = "Tesla Inc"
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
+# print(news_data)
+
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+news_params = {
+    "country": "us",
+    "category":"business",
+    "apiKey":news_api_key
+
+} 
+
+news_response = rq.get("https://newsapi.org/v2/top-headlines?", params=news_params)
+news_data = news_response.json()["articles"] # Type List
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
-
-#Optional: Format the SMS message like this: 
 def send_email():
     with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
         connection.login(user=EMAIL,password=EMAIL_APP_PASS)
-        # connection.sendmail(from_addr=,to_addrs=,msg=)
+        connection.sendmail(from_addr=EMAIL,to_addrs=send_to,msg=f"Subject: Headline:{news_data[0]["title"]} \n\n Brief:{news_data[0]["description"]}")
+
+#Optional: Format the SMS message like this: 
+
 
 """
 TSLA: 🔺2%
